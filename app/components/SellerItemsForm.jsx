30b72e18d565888fddefcc2f7e22bb16c873bbd3
oyingrace <oyingrace307@gmail.com>
2026-01-19 14:02:39 +0100

    chore: save 2026-01-19 14:02:39

--- app/components/SellerItemsForm.jsx
+++ app/components/SellerItemsForm.jsx
@@ -6,4 +6,4 @@ import React, { useState, useEffect } from "react";
  * Reusable form component for creating or editing a marketplace item.
  *
  * * Props:
-
+* - mode: "create" | "edit" (default: "create")
