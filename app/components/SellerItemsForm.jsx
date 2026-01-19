230a0845eba82863bdb50db740bc974764fa3183
oyingrace <oyingrace307@gmail.com>
2026-01-19 17:41:09 +0100

    chore: save 2026-01-19 17:41:09

--- app/components/SellerItemsForm.jsx
+++ app/components/SellerItemsForm.jsx
@@ -164,3 +164,4 @@ const SellerItemsForm = ({
           onChange={(e) => setImageUrl(e.target.value)}
           className={`w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 ${
             errors.imageUrl ? "border-red-500" : "border-gray-300"
+          }`}
