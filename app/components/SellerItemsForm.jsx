9ec54231390b515da35dc8e94105b0c80cd977ef
oyingrace <oyingrace307@gmail.com>
2026-01-19 14:32:58 +0100

    chore: save 2026-01-19 14:32:58

--- app/components/SellerItemsForm.jsx
+++ app/components/SellerItemsForm.jsx
@@ -109,3 +109,4 @@ const SellerItemsForm = ({
           value={name}
           onChange={(e) => setName(e.target.value)}
           className={`w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 ${
+            errors.name ? "border-red-500" : "border-gray-300"
