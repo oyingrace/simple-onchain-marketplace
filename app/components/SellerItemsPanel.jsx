783503c85f82dbc4918e04fde244fe1b7cabd889
oyingrace <oyingrace307@gmail.com>
2026-01-20 10:46:06 +0100

    chore: save 2026-01-20 10:46:06

--- app/components/SellerItemsPanel.jsx
+++ app/components/SellerItemsPanel.jsx
@@ -142,3 +142,4 @@ const SellerItemsPanel = ({
               <div
                 key={item.itemId}
                 className="border border-gray-200 rounded-md p-3 flex flex-col sm:flex-row sm:items-start sm:justify-between gap-3"
+              >
