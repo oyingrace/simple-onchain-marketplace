fd567994f893bad1aa12577c1595a57435b1ad44
oyingrace <oyingrace307@gmail.com>
2026-01-20 10:39:41 +0100

    chore: save 2026-01-20 10:39:41

--- app/components/SellerItemsPanel.jsx
+++ app/components/SellerItemsPanel.jsx
@@ -103,3 +103,4 @@ const SellerItemsPanel = ({
           <button
             type="button"
             onClick={() => setEditingItemId(null)}
+            className="mt-3 text-sm text-gray-500 hover:text-gray-700 underline"
