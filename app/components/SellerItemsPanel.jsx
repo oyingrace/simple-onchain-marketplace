164f6516ac826cede9c23468a3ea632497348238
oyingrace <oyingrace307@gmail.com>
2026-01-20 10:43:04 +0100

    chore: save 2026-01-20 10:43:04

--- app/components/SellerItemsPanel.jsx
+++ app/components/SellerItemsPanel.jsx
@@ -124,3 +124,4 @@ const SellerItemsPanel = ({
                 type="button"
                 onClick={onRefresh}
                 disabled={loading || submitting}
+                className="text-sm text-indigo-600 hover:text-indigo-800 underline disabled:text-gray-400"
