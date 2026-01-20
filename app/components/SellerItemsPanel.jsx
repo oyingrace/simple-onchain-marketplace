3645dcb2f1b806e5e589f84d60ee168aeaba81b6
oyingrace <oyingrace307@gmail.com>
2026-01-20 11:05:24 +0100

    chore: save 2026-01-20 11:05:24

--- app/components/SellerItemsPanel.jsx
+++ app/components/SellerItemsPanel.jsx
@@ -7,7 +7,7 @@ import SellerItemsForm from "./SellerItemsForm";
  * SellerItemsPanel
  * - Displays seller's items
  * - Provides create/edit form
- * - Expects callbacks from parent for data operations (fetch/create/update)
+ * - Expects callbacks from parent for data operations (fetch/create/update/remove)
  */
 const SellerItemsPanel = ({
   items = [],
