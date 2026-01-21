5eafeded9088441967431715168482d13e9df089
oyingrace <oyingrace307@gmail.com>
2026-01-21 12:18:05 +0100

    chore: save 2026-01-21 12:18:05

--- app/components/SellerDashboard.jsx
+++ app/components/SellerDashboard.jsx
@@ -199,3 +199,4 @@ const SellerDashboard = () => {
                 <button
                   onClick={() => registerSeller("New Seller")}
                   className="self-start bg-green-600 hover:bg-green-700 text-white font-medium py-2 px-4 rounded-md transition-colors duration-200"
+                >
