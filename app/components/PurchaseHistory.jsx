3b184560978197053f82621ad78f93c0c527ac5d
oyingrace <oyingrace307@gmail.com>
2026-01-21 18:42:53 +0100

    chore: save 2026-01-21 18:42:53

--- app/components/PurchaseHistory.jsx
+++ app/components/PurchaseHistory.jsx
@@ -159,7 +159,8 @@ const PurchaseHistory = () => {
               {purchases.map((purchase, idx) => (
                 <tr key={`${purchase.itemId}-${idx}`} className="hover:bg-gray-50">
                   <td className="px-4 py-3 whitespace-nowrap">
-                    <div className="font-medium text-gray-900">
+                    
+                  <div className="font-medium text-gray-900">
                       {purchase.itemName}
                     </div>
                     <div className="text-xs text-gray-500">#{purchase.itemId}</div>
