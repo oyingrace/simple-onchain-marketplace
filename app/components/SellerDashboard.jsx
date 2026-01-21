171ec76ff15d7da8349950020d62491869cf2273
oyingrace <oyingrace307@gmail.com>
2026-01-21 11:31:43 +0100

    chore: save 2026-01-21 11:31:43

--- app/components/SellerDashboard.jsx
+++ app/components/SellerDashboard.jsx
@@ -2,3 +2,5 @@
 
 import React, { useEffect, useMemo, useState } from "react";
 import SellerItemsPanel from "./SellerItemsPanel";
+import { useContract } from "../../lib/useContract.js";
+import { formatEthPrice } from "../../lib/contract.js";
