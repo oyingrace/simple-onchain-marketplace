eed2a2d830fded42beb7070c1b5ec2054cea1757
oyingrace <oyingrace307@gmail.com>
2026-01-21 12:23:09 +0100

    chore: save 2026-01-21 12:23:09

--- app/components/SellerDashboard.jsx
+++ app/components/SellerDashboard.jsx
@@ -260,3 +260,4 @@ const SellerDashboard = () => {
           <div className="flex items-center justify-between mb-4">
             <h3 className="text-lg font-semibold text-gray-900">
               Purchases (on-chain)
+            </h3>
