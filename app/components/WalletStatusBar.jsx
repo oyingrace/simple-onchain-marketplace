a3f3752bc0cf62d0f8e6b86b12c043b600b27c3b
oyingrace <oyingrace307@gmail.com>
2026-01-21 18:52:52 +0100

    chore: save 2026-01-21 18:52:52

--- app/components/WalletStatusBar.jsx
+++ app/components/WalletStatusBar.jsx
@@ -79,3 +79,4 @@ const WalletStatusBar = () => {
           <button
             onClick={connectWallet}
             disabled={isConnecting}
+            className="bg-white text-indigo-600 hover:bg-gray-100 disabled:bg-gray-300 font-medium py-2 px-4 rounded-md transition-colors duration-200"
