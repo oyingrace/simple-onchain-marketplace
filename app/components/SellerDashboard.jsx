54fd1f070b9d24c733b2fb940bd55d4db3cb0d17
oyingrace <oyingrace307@gmail.com>
2026-01-21 12:25:01 +0100

    chore: save 2026-01-21 12:25:01

--- app/components/SellerDashboard.jsx
+++ app/components/SellerDashboard.jsx
@@ -282,3 +282,4 @@ const SellerDashboard = () => {
               <table className="min-w-full divide-y divide-gray-200">
                 <thead>
                   <tr>
+                    <th className="px-3 py-2 text-left text-xs font-semibold text-gray-600">
