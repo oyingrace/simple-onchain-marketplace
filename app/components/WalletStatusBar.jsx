93b4cf8db1143a2512edc358ebf2f3a034267ba2
oyingrace <oyingrace307@gmail.com>
2026-01-21 18:56:10 +0100

    chore: save 2026-01-21 18:56:10

--- app/components/WalletStatusBar.jsx
+++ app/components/WalletStatusBar.jsx
@@ -119,3 +119,4 @@ const WalletStatusBar = () => {
       {message && (
         <div className="mt-3 pt-3 border-t border-white/20">
           <div className="flex items-center justify-between text-sm">
+            <span>{message}</span>
