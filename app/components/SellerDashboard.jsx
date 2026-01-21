21d14f99a95d09b6427a257f116fb0efe6484514
oyingrace <oyingrace307@gmail.com>
2026-01-21 11:38:36 +0100

    chore: save 2026-01-21 11:38:36

--- app/components/SellerDashboard.jsx
+++ app/components/SellerDashboard.jsx
@@ -85,3 +85,5 @@ const SellerDashboard = () => {
     );
     if (result?.success) {
       const newId =
+        result.itemId?.toString?.() ?? `TEMP-${Date.now().toString()}`;
+      setItems((prev) => [
