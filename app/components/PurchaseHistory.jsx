61a6ac5226e42c0ac69daa68cd34dc6ae66b0a8d
oyingrace <oyingrace307@gmail.com>
2026-01-21 18:43:36 +0100

    refactor: remove unnecessary whitespace in PurchaseHistory component

--- app/components/PurchaseHistory.jsx
+++ app/components/PurchaseHistory.jsx
@@ -159,7 +159,6 @@ const PurchaseHistory = () => {
               {purchases.map((purchase, idx) => (
                 <tr key={`${purchase.itemId}-${idx}`} className="hover:bg-gray-50">
                   <td className="px-4 py-3 whitespace-nowrap">
-                    
                   <div className="font-medium text-gray-900">
                       {purchase.itemName}
                     </div>
