9f3efe05361fd0e6e5a63218abe980955a5ade97
oyingrace <oyingrace307@gmail.com>
2026-01-21 18:22:11 +0100

    chore: save 2026-01-21 18:22:11

--- app/components/PurchaseHistory.jsx
+++ app/components/PurchaseHistory.jsx
@@ -68,3 +68,4 @@ const PurchaseHistory = () => {
       <div className="bg-white rounded-lg shadow border border-gray-100 p-6">
         <h2 className="text-xl font-bold text-gray-900 mb-4">
           Purchase History
+        </h2>
