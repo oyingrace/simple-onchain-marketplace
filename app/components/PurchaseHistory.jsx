7deec3c7e860481bb9ebb84d05b933dc53ec1137
oyingrace <oyingrace307@gmail.com>
2026-01-21 18:28:57 +0100

    chore: save 2026-01-21 18:28:57

--- app/components/PurchaseHistory.jsx
+++ app/components/PurchaseHistory.jsx
@@ -149,3 +149,4 @@ const PurchaseHistory = () => {
                 </th>
                 <th className="px-4 py-3 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                   Price
+                </th>
