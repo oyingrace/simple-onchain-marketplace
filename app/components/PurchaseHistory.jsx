0544f27eb4059cf1314f6297dfbcf44839711f21
oyingrace <oyingrace307@gmail.com>
2026-01-21 18:22:51 +0100

    chore: save 2026-01-21 18:22:51

--- app/components/PurchaseHistory.jsx
+++ app/components/PurchaseHistory.jsx
@@ -76,3 +76,4 @@ const PurchaseHistory = () => {
           <button
             onClick={connectWallet}
             disabled={isConnecting}
+            className="bg-indigo-600 hover:bg-indigo-700 disabled:bg-gray-400 text-white font-medium py-2 px-6 rounded-lg transition-colors duration-200"
