fbd33dc4c2a6298fef6fc48b139439647bdf17c6
oyingrace <oyingrace307@gmail.com>
2026-01-21 18:49:27 +0100

    chore: save 2026-01-21 18:49:27

--- app/components/WalletStatusBar.jsx
+++ app/components/WalletStatusBar.jsx
@@ -38,3 +38,4 @@ const WalletStatusBar = () => {
       // We need to get the provider from the contract
       // For now, we'll use a placeholder - in a real app you'd get this from the contract instance
       // This is a simplified version
+      setBalance("Loading...");
