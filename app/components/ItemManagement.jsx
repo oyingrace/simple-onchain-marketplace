d61a2f80fadab4d12a79a786797329580733fcb4
oyingrace <oyingrace307@gmail.com>
2026-01-21 14:57:34 +0100

    chore: save 2026-01-21 14:57:33

--- app/components/ItemManagement.jsx
+++ app/components/ItemManagement.jsx
@@ -61,3 +61,4 @@ const ItemManagement = () => {
   const handleCreate = async (values) => {
     const priceWithEth = ensureEthSuffix(values.price);
     const result = await createItem(
+      values.name,
