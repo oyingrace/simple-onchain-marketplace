62320382398570a5d641a6ea0d1987b59ba3ef7d
oyingrace <oyingrace307@gmail.com>
2026-01-21 14:51:44 +0100

    Add ItemManagement component and implement removeItem and getItemsBySeller functions in contract

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import ItemsCard from './components/ItemsCard';
 import SellerDashboard from './components/SellerDashboard';
+import ItemManagement from './components/ItemManagement';
 
 export default function Home() {
   return (
     <div className="space-y-10">
       <ItemsCard />
       <SellerDashboard />
+      <ItemManagement />
     </div>
   );
 }
