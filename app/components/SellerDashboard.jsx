48e9c11c0bd05666de1caf1af9e8b9ba0df2decf
oyingrace <oyingrace307@gmail.com>
2026-01-21 12:04:13 +0100

    chore: save 2026-01-21 12:04:13

--- app/components/SellerDashboard.jsx
+++ app/components/SellerDashboard.jsx
@@ -36,3 +36,4 @@ const SellerDashboard = () => {
 
   const [items, setItems] = useState([]);
   const [purchases, setPurchases] = useState([]);
+  const [loadingPurchases, setLoadingPurchases] = useState(false);
