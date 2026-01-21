55ec3eb296a3cf29d3b2528a9803cafcb312ffd1
oyingrace <oyingrace307@gmail.com>
2026-01-21 12:15:22 +0100

    chore: save 2026-01-21 12:15:22

--- app/components/SellerDashboard.jsx
+++ app/components/SellerDashboard.jsx
@@ -167,3 +167,4 @@ const SellerDashboard = () => {
       <div className="max-w-6xl mx-auto px-4 space-y-8">
         <div className="flex flex-col gap-2">
           <h2 className="text-2xl font-bold text-gray-900">
+            Seller Dashboard
