959d665ea6eaa734dabd5b29db3e6eb490c33667
oyingrace <oyingrace307@gmail.com>
2026-01-21 12:25:46 +0100

    chore: save 2026-01-21 12:25:46

--- app/components/SellerDashboard.jsx
+++ app/components/SellerDashboard.jsx
@@ -291,3 +291,4 @@ const SellerDashboard = () => {
                     <th className="px-3 py-2 text-left text-xs font-semibold text-gray-600">
                       Seller
                     </th>
+                    <th className="px-3 py-2 text-left text-xs font-semibold text-gray-600">
