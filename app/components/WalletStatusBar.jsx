ad1be2a715d88f93f9c2859c083f4ecbaec8661d
oyingrace <oyingrace307@gmail.com>
2026-01-21 18:52:16 +0100

    chore: save 2026-01-21 18:52:16

--- app/components/WalletStatusBar.jsx
+++ app/components/WalletStatusBar.jsx
@@ -72,3 +72,4 @@ const WalletStatusBar = () => {
     return (
       <div className="bg-gradient-to-r from-indigo-500 to-purple-600 text-white p-4 rounded-lg shadow-lg">
         <div className="flex items-center justify-between">
+          <div>
