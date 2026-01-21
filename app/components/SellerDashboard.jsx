42d7ecb4c90e7e2f38b28150d5b2dcc8e1cea709
oyingrace <oyingrace307@gmail.com>
2026-01-21 12:27:18 +0100

    chore: save 2026-01-21 12:27:18

--- app/components/SellerDashboard.jsx
+++ app/components/SellerDashboard.jsx
@@ -310,3 +310,4 @@ const SellerDashboard = () => {
                       </td>
                       <td className="px-3 py-2 text-xs text-gray-600">
                         {p.seller?.slice(0, 6)}...{p.seller?.slice(-4)}
+                      </td>
