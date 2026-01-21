f49dabd3ada829df849b52e097689a03a4482d51
oyingrace <oyingrace307@gmail.com>
2026-01-21 15:00:43 +0100

    chore: save 2026-01-21 15:00:42

--- app/components/ItemManagement.jsx
+++ app/components/ItemManagement.jsx
@@ -99,3 +99,4 @@ const ItemManagement = () => {
   return (
     <div className="bg-gray-50 py-10">
       <div className="max-w-5xl mx-auto px-4 space-y-6">
+        <div className="flex flex-col gap-2">
