46893a6c8de160da67aa0400554fd70849efa268
oyingrace <oyingrace307@gmail.com>
2026-01-21 18:54:39 +0100

    chore: save 2026-01-21 18:54:39

--- app/components/WalletStatusBar.jsx
+++ app/components/WalletStatusBar.jsx
@@ -101,3 +101,4 @@ const WalletStatusBar = () => {
           <div>
             <p className="text-xs opacity-90 mb-1">Network</p>
             <p className="text-sm font-semibold">{network}</p>
+          </div>
