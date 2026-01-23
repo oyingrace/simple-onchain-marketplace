90f1b9723af53db3d356aabf2c57d7c41ff92780
oyingrace <oyingrace307@gmail.com>
2026-01-23 12:54:36 +0100

    chore: save 2026-01-23 12:54:36

--- app/components/ActiveItemsList.jsx
+++ app/components/ActiveItemsList.jsx
@@ -141,3 +141,4 @@ const ActiveItemsList = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {items.map((item) => (
             <div
+              key={item.itemId}
