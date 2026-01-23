d88555dac7727725057edfb628eefbcc91a819e5
oyingrace <oyingrace307@gmail.com>
2026-01-23 12:54:05 +0100

    chore: save 2026-01-23 12:54:05

--- app/components/ActiveItemsList.jsx
+++ app/components/ActiveItemsList.jsx
@@ -135,3 +135,4 @@ const ActiveItemsList = () => {
         <div className="text-center py-12 bg-white rounded-lg border border-gray-200">
           <p className="text-gray-500">
             No active items found. Sellers can create items to get started!
+          </p>
