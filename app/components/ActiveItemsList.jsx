65f5eff78c01c426263d3613fb144d4a50355d0e
oyingrace <oyingrace307@gmail.com>
2026-01-23 12:53:39 +0100

    chore: save 2026-01-23 12:53:39

--- app/components/ActiveItemsList.jsx
+++ app/components/ActiveItemsList.jsx
@@ -130,3 +130,4 @@ const ActiveItemsList = () => {
       {loading ? (
         <div className="text-center py-12">
           <p className="text-gray-500">Loading items from blockchain...</p>
+        </div>
