039ea7ecfc1b94f160a00346ae013126d460262a
oyingrace <oyingrace307@gmail.com>
2026-01-23 12:57:30 +0100

    chore: save 2026-01-23 12:57:30

--- app/components/ActiveItemsList.jsx
+++ app/components/ActiveItemsList.jsx
@@ -176,3 +176,4 @@ const ActiveItemsList = () => {
                     {item.price}
                   </span>
                   <span
+                    className={`text-xs px-2 py-1 rounded-full ${
