0ff3a55853e812eb981aef37b3d6b3d70d6a5986
oyingrace <oyingrace307@gmail.com>
2026-01-23 12:43:09 +0100

    chore: save 2026-01-23 12:43:09

--- app/components/ActiveItemsList.jsx
+++ app/components/ActiveItemsList.jsx
@@ -4,3 +4,4 @@
 import React, { useEffect, useState } from "react";
 import { useContract } from "../../lib/useContract.js";
 import { formatEthPrice } from "../../lib/contract.js";
+import ItemDetailsModal from "./ItemDetailsModal.jsx";
