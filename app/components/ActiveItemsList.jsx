2350232fcbe07473eefd8e07f5427a67cd08db32
oyingrace <oyingrace307@gmail.com>
2026-01-23 12:56:28 +0100

    chore: save 2026-01-23 12:56:28

--- app/components/ActiveItemsList.jsx
+++ app/components/ActiveItemsList.jsx
@@ -163,3 +163,4 @@ const ActiveItemsList = () => {
               <div className="p-4">
                 <h3 className="text-base font-semibold text-gray-900 mb-2 line-clamp-2">
                   {item.name}
+                </h3>
