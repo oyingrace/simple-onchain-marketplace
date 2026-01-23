f1df47e0079fa8bc9e80ee015da5cd33a8a38674
oyingrace <oyingrace307@gmail.com>
2026-01-23 12:54:56 +0100

    chore: save 2026-01-23 12:54:56

--- app/components/ActiveItemsList.jsx
+++ app/components/ActiveItemsList.jsx
@@ -145,3 +145,4 @@ const ActiveItemsList = () => {
               className="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 overflow-hidden border border-gray-100 cursor-pointer"
               onClick={() => setSelectedItem(item)}
             >
+              {/* Image */}
